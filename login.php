81cba52e946b7b31f1e0c5ddd1e21cdfe32ce65a
Nisha khadka <120707560+nishaaaaaa1@users.noreply.github.com>
2024-06-01 17:51:30 +0545

    Add files via upload

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+</head>
+<body>
+
+    <form action="" method="post">
+        Email <input type="text" name="username" id="username">
+        <br>
+        <br>
+        password<input type=" password" name=" password" id=" password">
+        <br>
+       <br>
+                <input type= "submit" value="login">
+</form><br><br>
+do not have account <a href="signup.php">signup</a>
+    
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+</head>
+<body>
+    <form action="" method="post">
+    email<input type=" email" name=" email" id=" email">
+        <br><br>
+        password<input type=" password" name=" password" id=" password">
+        <br>
+        <br>
+    
+        Dob<input type="date" name="dob" id=" dob">
+        <br><br>
+        <input type="submit" value="signup">
+</form><br>
+already have account; <a href="login.php"> login</a>
+    
+</body>
+</html>
